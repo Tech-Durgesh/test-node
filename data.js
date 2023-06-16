bad7847dee3a48a1942c3b3a25cf3916178613e6
Durgesh Sharma <durgesh@kanhasoft.com>
2023-06-16 17:20:40 +0530

    Simple Api Example

---
+++ data.js
@@ -0,0 +1,22 @@
+const data = [
+  {
+    name: "Durgesh Sharma",
+    designation: "Sr. Software Developer",
+    email: "durgesh@kanhasoft.com",
+    mobile: "+91 97822-73248",
+  },
+  {
+    name: "Akhil Parmar",
+    designation: "Sr. Software Developer",
+    email: "akhil@kanhasoft.com",
+    mobile: "+91 12345-67890",
+  },
+  {
+    name: "Vijay Gohil",
+    designation: "DevOps",
+    email: "vijay@kanhasoft.com",
+    mobile: "+91 12345-67890",
+  },
+];
+
+module.exports = data;
--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const http = require("http");
-
+const data = require("./data");
 const myFn = (req, resp) => {
-    resp.write("Durgesh Sharma");
-    resp.end();
+  resp.writeHead(200, { "Content-type": "applicationjson" });
+  resp.write(
+    JSON.stringify(data)
+  );
+  resp.end();
+};
 
-}
-
-
-http.createServer(myFn).listen(3200);
+http.createServer(myFn).listen(5000);

d178d5f4ae4112bcfc8beb980de8418508582a9f
Durgesh Sharma <durgesh@kanhasoft.com>
2023-06-29 18:23:31 +0530

    File upload & os info

--- index.js
+++ index.js
@@ -1,9 +1,21 @@
 require('./mongoose');
 const StudentModel = require('./Schema/students');
 const express = require('express');
+const multer = require('multer');
 const app = express();
 
 app.use(express.json());
+
+const fileUpload = multer({
+    storage:multer.diskStorage({
+        destination: function(req, file, callBack){
+            callBack(null, 'uploads/images');
+        },
+        filename: function(req, file, callBack){
+            callBack(null, file.fieldname+'_'+Date.now()+'.jpg');
+        }
+    })
+}).single("file");
 
 app.post('/insert', async (req, resp)=>{
     let record = new StudentModel(req?.body);
@@ -28,6 +40,10 @@ app.delete('/delete/:_id', async (req, resp)=>{
     let result = await StudentModel.deleteOne(req?.params);
     resp.send(result);
 });
+
+app.post('/file-upload', fileUpload, (req, resp) => {
+    resp.send('Uploaded successfully!');
+})
 
 app.get('/search/:key', async (req, resp)=>{
     let result = await StudentModel.find({
